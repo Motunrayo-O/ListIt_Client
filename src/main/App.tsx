2dcb654086326a23b25c17c33976696d4b6cebed
motunrayo-o <motunrayo.ogunyinka@gmail.com>
2023-10-30 12:35:07 +0000

    initial commit - Add react app and header component

---
+++ src/main/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header subtitle="Making your dream home a reality" />
+    </div>
+  );
+}
+
+export default App;
